6bee40977b649aa0fc6cfadb3dcdf5fff8389005
juanmanuelrodriguezcarreño <juanmanuelrodriguez8807@gmail.com>
2025-12-04 17:24:26 -0500

    desarrollando puntoD no terminado

---
+++ puntoD/ejercicio1/ejercicio1.js
@@ -0,0 +1,27 @@
+/*1. Acceso a un sistema
+Un sistema debe permitir el acceso a un usuario si cumple estas condiciones:
+• El nombre de usuario es "admin".
+• La contraseña es "1234".
+Si no cumple las dos condiciones, debe mostrar "Acceso denegado".
+Pregunta de análisis: ¿Qué operador lógico usarías para verificar que se cumplan
+ambas condiciones?*/
+
+// importar el módulo prompt-sync
+import promptSync from 'prompt-sync';
+// crear una instancia del prompt
+const prompt = promptSync();
+
+//solicitamos el nombre de usuario y su contraseña
+let usuario = prompt("ingrese su nombre de usuario por favor:")
+let contraseña = prompt("ingrese su contraseña por favor:")
+
+// hacemos una condicional, usamos el if si el usuario == admin y contraseña == 1234 se le permite el acceso al sistema y el else si usario no es igual a usuario y contraseña no es igual ala contraseña le imprime un mensaje de acceso denegado 
+if (usuario == "admin" && contraseña == "1234"){ //&& sirve para evaluar dos o mas expresiones
+    console.log("acceso permitido")
+    console.log("bienvenido al sistema ")
+}
+else{
+    console.log("acceso denegado")
+    console.log("no puede acceder al sistema")
+
+}
---
+++ puntoD/ejercicio2/ejercicio2.js
@@ -0,0 +1,13 @@
+/*2. Calcular precio de entradas al cine
+Un cine aplica las siguientes reglas:
+• Si el cliente tiene menos de 12 años, el valor de la entrada es 5000.
+• Si tiene entre 12 y 18 años, el valor es 8000.
+• Si es mayor de 18, el valor es 10000.
+
+GFPI-F-135 V04
+
+• Si el cliente es estudiante (condición adicional), se le aplica un descuento del 20%
+al valor de la entrada.
+
+Pregunta de análisis: ¿Qué estructura condicional usarías para resolver este caso: if-
+else if-else o switch? ¿Por qué?*/
